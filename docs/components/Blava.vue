41e169e0bd66933a4b22cde4e92b61e60413fcdd
Tony Ketcham <tonyketcham@gmail.com>
2021-10-05 18:18:26 -0500

    add(docs): blava vue component props

--- docs/components/Blava.vue
+++ docs/components/Blava.vue
@@ -1,17 +1,31 @@
 <template>
-  <canvas ref="canvas"></canvas>
+  <canvas ref="canvas" :width="width" :height="height"></canvas>
 </template>
 
 <script>
 import { Blava } from '../../src/main.js';
 export default {
+  props: {
+    settings: {
+      type: Object,
+      default: () => {
+        return {
+          movementSpeed: 'medium',
+          style: 'blob',
+        };
+      },
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 400,
+    },
+  },
   mounted() {
-    new Blava(this.$refs.canvas, {
-      movementSpeed: 'medium',
-      style: 'blob',
-    });
+    new Blava(this.$refs.canvas, this.settings);
   },
 };
 </script>
-
-<style></style>
--- docs/index.md
+++ docs/index.md
@@ -1,9 +1,9 @@
-<Blava/>
-
-# Blava
+![Blava](/assets/blava-logo.svg)
 
 Performant artful blob generator using canvas 📋
 
 - [Getting Started](./guide/getting_started.md)
 - [Advanced Configuration](./guide/advanced_configuration.md)
 - [Use Cases](./guide/use_cases.md)
+
+<Blava/>
